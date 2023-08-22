ed0b5102bbca11475aaeffc27599732b8092a448
MD IMRAN HOSSAIN <you@example.com>
2023-08-22 12:56:50 +0600

    init project

---
+++ simple-gradle-app/src/main/java/com/imran/SimpleApplication.java
@@ -0,0 +1,10 @@
+package com.imran;
+
+public class SimpleApplication {
+
+    public static void main(String[] args){
+
+        System.out.println("SimpleGradleApplication run successfully done.");
+
+    }
+}
