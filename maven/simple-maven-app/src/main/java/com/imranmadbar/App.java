5759c88e4b9d7b92b67b1c4c7a3908a841a89dfc
MD IMRAN HOSSAIN <you@example.com>
2023-08-24 15:38:58 +0600

    init maven simple project

---
+++ maven/simple-maven-app/src/main/java/com/imranmadbar/App.java
@@ -0,0 +1,13 @@
+package com.imranmadbar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hellow this is a smiple Maven(Apache Maven 3.6.3) Project init using console" );
+    }
+}
